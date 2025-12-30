6a366947fa5a74f02c0db4fb55dd4bea140ee941
NistNerj <131333192+NistNerj@users.noreply.github.com>
2025-12-30 06:45:49 +0400

    Update sorts.h

--- 11lab/sorts.h
+++ 11lab/sorts.h
@@ -14,8 +14,9 @@ void centeredAsc(int a[], int n);
 void centeredDesc(int a[], int n);
 
 
-void quadraticAsc(int a[], int n);
-void quadraticDesc(int a[], int n);
+void quadraticAsc(int* A, int N);
+void quadraticDesc(int* A, int N);
 
 void quickAsc(int a[], int n);
 void quickDesc(int a[], int n);
+
